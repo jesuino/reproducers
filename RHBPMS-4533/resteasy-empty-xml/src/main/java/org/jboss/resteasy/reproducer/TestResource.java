1c68c3d7a331fe075eb015951903eb7a12ce9537
William Antonio <william.fatecsjc@gmail.com>
2016-12-15 15:12:35 -0200

    trying a method without param

--- RHBPMS-4533/resteasy-empty-xml/src/main/java/org/jboss/resteasy/reproducer/TestResource.java
+++ RHBPMS-4533/resteasy-empty-xml/src/main/java/org/jboss/resteasy/reproducer/TestResource.java
@@ -4,12 +4,25 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 
 
+/**
+ * @author wsiqueir
+ *
+ */
 @Path("/")
 public class TestResource {
 
 	@POST
 	public void test(Test test) {
 		System.out.println("test: " + test);
+	}
+	
+	
+	/**
+	 * adding this is not working as well
+	 */
+	@POST
+	public void test() {
+		System.out.println("test without body");
 	}
 	
 }
